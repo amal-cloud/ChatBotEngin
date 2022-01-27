a38aa4233b32811a0479f9040b8142df9896c717
amal amal <chebilamal820@gmail.com>
2022-01-27 20:30:00 +0100

    setups chatEngine

---
+++ chatbot_app/src/App.js
@@ -0,0 +1,12 @@
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+export default function App() {
+  return (
+    <ChatEngine
+      height="100vh"
+      userName="Admin"
+      userSecret="Admin"
+      projectID="268ea750-84f8-4114-8585-24c67ff6082c"
+    />
+  );
+}
